512577165eefe2b2525b6c47ad54d0d52ea91878
nguen1225 <nguen1225@gmail.com>
2021-11-09 02:08:01 +0900

    home追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,19 @@
 @extends('layouts.detail')
 @section('body')
 
+
+<div class="max-w-md mx-auto mt-6">
+    <h2>
+        {{ $user->name }}
+    </h2>
+    <h3>
+        パスワード変更
+    </h3>
+    <h3>
+        スケジュールのジャンルを増やす。
+    </h3>
+    <h3>
+        身体の情報を記録する
+    </h3>
+</div>
 @endsection
