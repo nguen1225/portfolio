69172d0969592fadaf8a0dc21a90a7c9b363657c
nguen1225 <nguen1225@gmail.com>
2021-11-14 14:04:42 +0900

    パスワードの長さをバリデーションする

---
+++ app/Rules/Password/CharacterLengthRule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Rules\Password;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CharacterLengthRule implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (mb_strlen($value) < 8)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
