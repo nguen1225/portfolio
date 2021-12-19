58a6670c4127dc416281a1ba30bf98f950c216f3
nguen1225 <nguen1225@gmail.com>
2021-12-20 00:42:38 +0900

    色々デザイン修正

--- resources/js/UI/monthButton.js
+++ resources/js/UI/monthButton.js
@@ -1,35 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     const today = new Date();
     const get_date = new Date(today.getFullYear(), today.getMonth(), 1);
-    const get_prev = document.querySelector('#prev')
-    const get_next = document.querySelector('#next')
+    const get_prev = document.querySelectorAll('#prev')
+    const get_next = document.querySelectorAll('#next')
 
     // 前の月表示
-    get_prev.addEventListener('click', prev);
-    function prev(){
-        get_date.setMonth(get_date.getMonth() - 1);
-        showProcess(get_date);
-    }
+    get_prev.forEach(item => {
+        item.addEventListener('click', prev);
+        function prev(){
+            get_date.setMonth(get_date.getMonth() - 1);
+            showProcess(get_date);
+        }
+    });
 
     // 次の月表示
-    get_next.addEventListener('click', next);
-    function next(){
-        get_date.setMonth(get_date.getMonth() + 1);
-        showProcess(get_date);
-    }
+    get_next.forEach(item => {
+        item.addEventListener('click', next);
+        function next(){
+            get_date.setMonth(get_date.getMonth() + 1);
+            showProcess(get_date);
+        }
+    });
 
     const year = today.getFullYear();
     const month = today.getMonth();
-    const get_class = document.querySelector('#month')
-    document.querySelector('#month').innerHTML = year + "年 " + (month + 1) + "月";
-    get_class.setAttribute('value', year + "/" + (month + 1));
+    const get_class = document.querySelectorAll('#month')
+    get_class.forEach(item => {
+        item.innerHTML = year + "年 " + (month + 1) + "月";
+        item.setAttribute('value', year + "/" + (month + 1));
+    });
 
     // ボタンをクリックで切り替え
     function showProcess(date) {
         const year = date.getFullYear();
         const month = date.getMonth();
-        const get_class = document.querySelector('#month')
-        get_class.innerHTML = year + "年 " + (month + 1) + "月";
-        get_class.setAttribute('value', year + "/" + (month + 1));
+        const get_class = document.querySelectorAll('#month')
+        get_class.forEach(item => {
+            item.innerHTML = year + "年 " + (month + 1) + "月";
+            item.setAttribute('value', year + "/" + (month + 1));
+        });
     }
 })
