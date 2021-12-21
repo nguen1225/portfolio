717a2ade5bd3ea9f9aba3db05557be0ace4a4020
nguen1225 <nguen1225@gmail.com>
2021-12-21 13:48:53 +0900

    スマホデザイン修正

--- resources/views/Vital/components/show-detail.blade.php
+++ resources/views/Vital/components/show-detail.blade.php
@@ -1,5 +1,5 @@
-<div class="bg-white px-4 py-5 sm:px-2 border-b border-gray-200">
-    <h4 class="underline text-xl">
+<div class="bg-white px-4 py-5 sm:px-2 border-b border-gray-200 rounded-lg">
+    <h4 class="underline text-xl font-semibold">
         備考
     </h4>
     <p class="break-words mb-5 border border-black mt-3 p-4">
@@ -8,27 +8,27 @@
     <table class="max-w-md w-full border-separate border border-black ...">
         <tbody>
             <tr>
-            <td class="p-1.5 border border-black ...">身長</td>
+            <td class="p-1.5 font-bold border border-black ...">身長</td>
             <td class="p-1.5 border border-black ...">{!! nl2br(htmlspecialchars($post_detail->height)) !!}cm</td>
             </tr>
             <tr>
-            <td class="p-1.5 border border-black ...">体重</td>
+            <td class="p-1.5 font-bold border border-black ...">体重</td>
             <td class="p-1.5 border border-black ...">{!! nl2br(htmlspecialchars($post_detail->body_weight)) !!}kg</td>
             </tr>
             <tr>
-            <td class="p-1.5 border border-black ...">最高血圧</td>
+            <td class="p-1.5 font-bold border border-black ...">最高血圧</td>
             <td class="p-1.5 border border-black ...">{!! nl2br(htmlspecialchars($post_detail->max_blood_pressure)) !!}mmHg</td>
             </tr>
             <tr>
-            <td class="p-1.5 border border-black ...">最低血圧</td>
+            <td class="p-1.5 font-bold border border-black ...">最低血圧</td>
             <td class="p-1.5 border border-black ...">{!! nl2br(htmlspecialchars($post_detail->min_blood_pressure)) !!}mmHg</td>
             </tr>
             <tr>
-            <td class="p-1.5 border border-black ...">平均血圧</td>
+            <td class="p-1.5 font-bold border border-black ...">平均血圧</td>
             <td class="p-1.5 border border-black ...">{!! nl2br(htmlspecialchars($avg_blood_pressure)) !!}mmHg</td>
             </tr>
             <tr>
-            <td class="p-1.5 border border-black ...">心拍数(1分間)</td>
+            <td class="p-1.5 font-bold border border-black ...">心拍数(1分間)</td>
             <td class="p-1.5 border border-black ..."> {!! nl2br(htmlspecialchars($post_detail->heart_rate)) !!}回</td>
             </tr>
         </tbody>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,26 +4,29 @@
     <div class="min-h-full flex items-center justify-center bg-gray-700 p-6 rounded-lg shadow-2xl">
         <div class="max-w-md w-full space-y-8">
         <div>
-            <h2 class="text-center text-3xl font-extrabold text-gray-50">
-                {{ $user->name }}
-            </h2>
+            <p class="text-center  mb-5">
+                <span class="text-3xl font-extrabold text-gray-50">
+                    {{ $user->name }}
+                </span>
+                <span class="text-gray-50 font-bold">さん</span>
+            </p>
             <p class="mt-2 text-gray-600 text-center">
-            <a href="{{ route('password.send-email') }}" class=" text-gray-50 hover:text-purple-200">
+            <a href="{{ route('password.send-email') }}" class=" text-gray-50 hover:text-purple-200 font-bold">
                 パスワード変更
             </a>
             </p>
             <p class="mt-2 text-gray-600 text-center">
-            <a href="{{ route('genre') }}" class=" text-gray-50 hover:text-purple-200">
+            <a href="{{ route('genre') }}" class=" text-gray-50 hover:text-purple-200 font-bold">
                 スケジュールのジャンルを増やす
             </a>
             </p>
             <p class="mt-2 text-gray-600 text-center">
-            <a href="{{ route('schedule') }}" class=" text-gray-50 hover:text-purple-200">
+            <a href="{{ route('schedule') }}" class=" text-gray-50 hover:text-purple-200 font-bold">
                 スケジュールを作成する
             </a>
             </p>
             <p class="mt-2 text-gray-600 text-center">
-            <a href="{{ route('vital') }}" class=" text-gray-50 hover:text-purple-200">
+            <a href="{{ route('vital') }}" class=" text-gray-50 hover:text-purple-200 font-bold">
                 身体の情報を記録する
             </a>
             </p>
