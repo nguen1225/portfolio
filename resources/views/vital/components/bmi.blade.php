01399941bed4128c99343ef820b892715535b0d6
nguen1225 <nguen1225@gmail.com>
2021-12-23 01:12:30 +0900

    bmiが0の時は表をハイライトしない

--- resources/views/vital/components/bmi.blade.php
+++ resources/views/vital/components/bmi.blade.php
@@ -29,7 +29,34 @@
                     <th class="ont-semibold text-white bg-gray-700 border border-black ...">肥満度</th>
                 </tr>
             </thead>
-            @if ($bmi < 18.5)
+            @if ($bmi === 0)
+            <tbody>
+                <tr>
+                    <td class="p-0.5 font-bold border border-black ...">18.5未満</td>
+                    <td class="p-0.5 border border-black ...">低体重</td>
+                </tr>
+                <tr>
+                    <td class="p-0.5 font-bold border border-black ...">18.5〜25未満</td>
+                    <td class="p-0.5 border border-black ...">普通体重</td>
+                </tr>
+                <tr>
+                    <td class="p-0.5 font-bold border border-black ...">25〜30未満</td>
+                    <td class="p-0.5 border border-black ...">肥満(1度)</td>
+                </tr>
+                <tr>
+                    <td class="p-0.5 font-bold border border-black ...">30〜35未満</td>
+                    <td class="p-0.5 border border-black ...">肥満(2度)</td>
+                </tr>
+                <tr>
+                    <td class="p-0.5 font-bold border border-black ...">35〜40未満</td>
+                    <td class="p-0.5 border border-black ...">肥満(3度)</td>
+                </tr>
+                <tr>
+                    <td class="p-0.5 font-bold border border-black ...">40以上</td>
+                    <td class="p-0.5 border border-black ...">肥満(4度)</td>
+                </tr>
+            </tbody>
+            @elseif ($bmi < 18.5)
             <tbody>
                 <tr>
                     <td class="bg-blue-200 p-0.5 font-bold border border-black ...">18.5未満</td>
