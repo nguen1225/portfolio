9db9fd1d68c843fb22dcee478e4b146593a0ca09
nguen1225 <nguen1225@gmail.com>
2022-01-04 00:47:48 +0900

    ヘッダーのデザイン修正

--- resources/js/UI/modal.js
+++ resources/js/UI/modal.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = document.querySelector('#modal_content');
     const button_close = document.querySelector('.modal_close');
 
+    const dashboard_modal_open = document.querySelector('#dashboard_modal_open');
+    const dashboard_modal = document.querySelector('#dashboard_modal_content');
+    const dashboard_button_close = document.querySelector('.dashboard_modal_close');
+
     if (modal) {
         // 表示
         modal_open.addEventListener('click', modalOpen);
@@ -26,6 +30,36 @@ document.addEventListener('DOMContentLoaded', () => {
         function outsideClose(e) {
             if(e.target == modal) {
                 location.reload();
+            }
+        }
+    }
+
+    if (dashboard_modal) {
+        // 表示
+        dashboard_modal_open.addEventListener('click', dashboard_modalOpen);
+        function dashboard_modalOpen() {
+            dashboard_modal.style.display = 'block';
+            dashboard_modal_open.style.display = 'none';
+        }
+
+        // バリデーションに引っ掛かったら表示
+        if(document.querySelector('.error')) {
+            dashboard_modalOpen();
+        }
+
+        // 閉じる
+        dashboard_button_close.addEventListener('click', dashboard_modalClose);
+        function dashboard_modalClose() {
+            dashboard_modal.style.display = 'none';
+            dashboard_modal_open.style.display = 'flex';
+        }
+
+        // どこをクリックしてもイベントを閉じる
+        addEventListener('click', dashboard_outsideClose);
+        function dashboard_outsideClose(e) {
+            if(e.target == dashboard_modal) {
+                dashboard_modal.style.display = 'none';
+                dashboard_modal_open.style.display = 'flex';
             }
         }
     }
--- resources/views/Schedule/components/calendar.blade.php
+++ resources/views/Schedule/components/calendar.blade.php
@@ -1,3 +1,3 @@
-<div class="calendar_width bg-white mb-10">
+<div class="calendar_width bg-white mb-10 mt-11 md:mt-0 lg:mt-0">
     <div id='calendar'></div>
 </div>
