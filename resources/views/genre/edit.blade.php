f57934d690087985ab07ed4d9403b464d5971dbd
nguen1225 <nguen1225@gmail.com>
2022-01-04 16:42:28 +0900

    ジャンル名変更機能追加

---
+++ resources/views/genre/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.detail')
+@section('body')
+<div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mt-20">
+    <div class="flex items-center justify-center bg-gray-700 p-6 rounded-lg shadow-2xl width-36">
+        <div class="max-w-md lg:w-96 md:w-96 width-100 space-y-8">
+            <div>
+                <h2>
+                    ジャンル名変更
+                </h2>
+                <p class="mt-2 text-sm text-gray-600 text-center">
+                    <a class="explanation_white">
+                        ジャンル名を変更する。
+                    </a>
+                </p>
+                @if (session('flash_message'))
+                <p class="mt-2 text-sm text-gray-600 text-center">
+                    <a class="explanation_white error">
+                        {!! nl2br(session('flash_message')) !!}
+                    </a>
+                </p>
+                @endif
+            </div>
+            @include('genre.components.edit-form')
+        </div>
+    </div>
+</div>
+@endsection
